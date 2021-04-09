f72fc6b97e505c0779481126cf90fb0e46fc5e9c
Jose Ballesteros <josebp0107@gmail.com>
2021-04-09 18:16:47 -0500

    I changed xpath's to shorter ones

--- start_browser.py
+++ start_browser.py
@@ -6,25 +6,16 @@ from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 
 
-
 class StartBrowser(object):
     def __init__(self, driver):
         self._driver = driver
         self._url = 'https://www.google.com/search?q=site%3Alinkedin.com%2Fin%2F+AND+%22ingenieria+de+sistemas%22+AND+%22Corporacion+Universitaria+del+Caribe%22&source=hp&ei=GvddYP_FN9KQ5gLItJ_QBw&iflsig=AINFCbYAAAAAYF4FKsf4407zMvunBoCXNyefbhjDpZqA&oq=site%3Alinkedin.com%2Fin%2F+AND+%22ingenieria+de+sistemas%22+AND+%22Corporacion+Universitaria+del+Caribe%22&gs_lcp=Cgdnd3Mtd2l6EANQiSFY5kBg5URoBXAAeAGAAZEBiAHqA5IBAzAuNJgBAKABAqABAaoBB2d3cy13aXqwAQA&sclient=gws-wiz&ved=0ahUKEwj_upnenM7vAhVSiFkKHUjaB3oQ4dUDCAc&uact=5'
-        self.search_locator = 'q'
-
-
 
     # Verifica que el sitio web de google haya cargado de forma correcta
     @property
     def is_loaded(self):
         WebDriverWait(self._driver, 10).until(EC.presence_of_element_located((By.NAME, 'q')))
         return True
-
-    @property
-    def keyword(self):
-        input_field = self._driver.find_element_by_name('q')
-        return input_field.get_attribute('value')
 
     # Llamada a la URL
     def open(self):
@@ -41,10 +32,10 @@ class StartBrowser(object):
     def get_link_name_profile(self):
         try:
             for profile in range(10):
-                get_profile = self._driver.find_element_by_xpath(f'/html/body/div[7]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div/div[{profile + 1}]/div/div[1]/a').get_attribute('href')
-                get_name = self._driver.find_element_by_xpath(f'/html/body/div[7]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div/div[{profile + 1}]/div/div[1]/a/h3').text
+                url_profile = self._driver.find_element_by_xpath(f'//div[@id="rso"]/div/div[{profile+1}]//a').get_attribute('href')
+                get_name = self._driver.find_element_by_xpath(f'//div[@id="rso"]/div/div[{profile+1}]//h3').text
                 get_name = get_name[:get_name.find("-") - 1]
-                print(f'{get_profile} -> {get_name} , {profile + 1}')
+                print(f'{url_profile} -> {get_name} , {profile + 1}')
             print("#" * 100)
         except NoSuchElementException as ex:
             print(ex.msg)
@@ -52,7 +43,7 @@ class StartBrowser(object):
     def login(self):
 
         try:
-            self._driver.find_element_by_css_selector('body > main > div > div > form.join-form > section > p > button').click()
+            self._driver.find_element_by_xpath('//p[starts-with(@class, "join")]/button').click()
 
             with open('account.txt', 'r') as f:
                 line = f.readlines()
@@ -77,8 +68,8 @@ class StartBrowser(object):
         try:
             name = self._driver.find_element_by_xpath('//li[@class="inline t-24 t-black t-normal break-words"]').text
             career = self._driver.find_element_by_xpath('//h2[@class="mt1 t-18 t-black t-normal break-words"]').text
-            count_elements_experience = len(self._driver.find_elements_by_xpath('//section[@id="experience-section"]/ul/li'))
-            for i in range(count_elements_experience):
+            elements_experience = len(self._driver.find_elements_by_xpath('//section[@id="experience-section"]/ul/li'))
+            for i in range(elements_experience):
                 experience_position = self._driver.find_element_by_xpath(f'//section[@id="experience-section"]/ul/li[{i+1}]//h3').text
                 experience_company = self._driver.find_element_by_xpath(f'//section[@id="experience-section"]/ul/li[{i+1}]//p[contains(@class, "pv-entity__secondary-title t-14")]').text
                 experience_date = self._driver.find_element_by_xpath(f'//section[@id="experience-section"]/ul/li[{i+1}]//h4[contains(@class, "pv-entity__date-range")]/span[not(@class)]').text
@@ -91,7 +82,7 @@ class StartBrowser(object):
             data = {
                 'name': name,
                 'career': career,
-                'element_experience': count_elements_experience,
+                'element_experience': elements_experience,
                 'experience': experience
             }
             parse_json = json.dumps(data, indent=4)
@@ -109,7 +100,7 @@ class StartBrowser(object):
             self.get_link_name_profile()
 
             for profile in range(10):
-                profile_student = self._driver.find_element_by_xpath(f'/html/body/div[7]/div/div[9]/div[1]/div/div[2]/div[2]/div/div/div/div[{profile + 1}]/div/div[1]/a')
+                profile_student = self._driver.find_element_by_xpath(f'//div[@id="rso"]/div/div[{profile+1}]//a')
 
                 try:
                     if profile == 0:
