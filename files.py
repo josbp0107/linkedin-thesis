8526bd511e3ed55e44316523a8816e576ca255db
Jose Ballesteros <josebp0107@gmail.com>
2021-04-21 17:11:15 -0500

    I created new file which contains all methods about creation, delete, and read files

---
+++ files.py
@@ -0,0 +1,36 @@
+import os
+
+
+class Files:
+
+    # Write data in json file
+    def write_file(self, data):
+        with open("data.json", "a+", encoding="utf-8") as f:
+            f.write(f'{data},')
+
+    # Delete json file when its create before to run the project
+    def delete_json(self):
+        if os.path.exists("data.json"):
+            return os.remove("data.json")
+
+    # Method to create json file that start with "[" for it take format of json
+    def create_json_file(self):
+        with open("data.json", "x", encoding="utf-8") as f:
+            f.write("[\n")
+            f.close()
+
+    # Open data.json and read, then validate if found
+    def close_json_file(self):
+        # If not exist file, show error and pass to next line, when read data.json file and then write in new file
+        # called new_data.json
+        try:
+            os.remove("new_data.json")
+        except FileNotFoundError as fn:
+            print(fn)
+
+        with open("data.json", "r+", encoding="utf-8") as input:
+            with open("new_data.json", "a+", encoding="utf-8") as f:
+                for line in input:
+                    if line != "},":
+                        f.write(line)
+                f.write("}]")
