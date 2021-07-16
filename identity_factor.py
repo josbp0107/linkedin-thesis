c00edbc6c25997e5b2ebb6dad1bdce8f43fa3ce3
josebp <josebp0107@gmail.com>
2021-07-16 11:19:39 -0500

    Doing classification of data

--- identity_factor.py
+++ identity_factor.py
@@ -12,8 +12,8 @@ FILE_CSV_EDUCATION = 'dataprocess/data_process_education.csv'
 FILE_CSV_CERTIFICATION = 'dataprocess/data_process_certification.csv'
 
 # List to compare
-LIST_ING_DESARROLLO_ANALISIS = ['desarrollador', 'ingeniero de sistemas', 'ingeniero', 'sistemas', 'freelance']
-LIST_ADM_BD = ['dato', 'datos']
+LIST_ING_DESARROLLO_ANALISIS = ['desarrollador', 'ingeniero de sistemas', 'ingeniero', 'sistemas', 'freelance', 'programador', 'web', 'developer', 'software', 'ingenieria']
+"""LIST_ADM_BD = ['dato', 'datos']
 LIST_ADM_REDES = ['redes', 'administrador de redes', 'administrador', 'soporte de red', 'red']
 LIST_ING_SOPORTE = ['soporte', 'mantenimiento']
 LIST_ADM_SERIVICIO = ['administrador de servicio', 'servicio', 'informaticos', 'informatico', 'servicio informatico', 'servicios informaticos']
@@ -21,7 +21,7 @@ LIST_DEV_SOLUCIONES = ['desarrolador de soluciones', 'soluciones']
 DEV_SIS_INFORMATICOS = ['sistemas informaticos', 'desarrollador de soluciones informaticos']
 INVESTIGADOR = ['investigador']
 GESTOR_PROYECTO = ['gestor', 'gestor de proyectos', 'gestora de proyectos']
-
+"""
 
 def fact_ident_experience(filecsv):
     """
@@ -131,10 +131,12 @@ def fact_ident_certifications(filecsv):
 
 def data_classification():
     desarrollador, admin_bd, admin_red, soporte, admin_servicio, dev_soluciones, dev_sistemas, investigador, gestor_proyec = [], [], [], [], [], [], [], [], []
-    count_desarrollador, count_admin_bd, count_admin_red = 0, 0, 0
+    count_desarrollador, count_admin_bd, count_admin_red, count_soporte, count_adm_servicio, count_dev_solucion, count_dev_sistemas, count_investigador, count_gest_proyect = 0, 0, 0, 0, 0, 0, 0, 0, 0
+    contador_total = 0
     with open(FILE_CSV_EXPERIENCE, "r", encoding="utf-8") as file_csv:
         reader = csv.DictReader(file_csv)
         for row in reader:
+            contador_total += 1
             row_res = row["responsibility"].lower().split(" ")
             if row_res[0] in LIST_ING_DESARROLLO_ANALISIS and not row_res.__contains__("bases"):
                 count_desarrollador += 1
@@ -143,10 +145,28 @@ def data_classification():
                 count_admin_bd += 1
                 admin_bd.append(row["responsibility"])
             elif row_res.__contains__('redes') or row_res.__contains__('red'):
-                count_admin_red +=1
+                count_admin_red += 1
                 admin_red.append(row["responsibility"])
+            elif row_res.__contains__('soporte') or row_res.__contains__('tecnico') and not row_res.__contains__("desarrollador") and not row_res.__contains__("desarrollo"):
+                count_soporte += 1
+                soporte.append(row["responsibility"])
+            elif row_res.__contains__('administrador') and not row_res.__contains__("bases") and not row_res.__contains__("red"):
+                count_adm_servicio += 1
+                admin_servicio.append(row["responsibility"])
+            elif row_res.__contains__("soluciones") or row_res.__contains__("arquitecto"):
+                count_dev_solucion += 1
+                dev_soluciones.append(row["responsibility"])
+            elif row_res.__contains__("informatico") or row_res.__contains__("informatica") or row_res.__contains__("sistemas") or row_res.__contains__("webmaster"):
+                count_dev_sistemas += 1
+                dev_sistemas.append(row["responsibility"])
+            elif row_res.__contains__("docente") or row_res.__contains__("investigador"):
+                count_investigador += 1
+                investigador.append(row["responsibility"])
+            elif row_res.__contains__('proyecto') or row_res.__contains__('gestor'):
+                count_gest_proyect += 1
+                gestor_proyec.append(row["responsibility"])
 
-    print("*"*20, "Total de desarrollador", "*"*20)
+    print("*"*20, "Total de desarrollador: ", "*"*20)
     print(count_desarrollador)
     print("*"*20, " DESARROLLADOR ", "*"*20)
     print(desarrollador)
@@ -160,6 +180,39 @@ def data_classification():
     print(count_admin_red)
     print("*" * 20, " ADMINISTRADOR DE REDES ", "*" * 20)
     print(admin_red)
+
+    print("*" * 20, "Total de Soporte: ", "*" * 20)
+    print(count_soporte)
+    print("*" * 20, " SOPORTE ", "*" * 20)
+    print(soporte)
+
+    print("*" * 20, "Total de Administrador de servicios informaticos: ", "*" * 20)
+    print(count_adm_servicio)
+    print("*" * 20, " ADMINISTRADOR DE SERVICIOS INFORMATICOS", "*" * 20)
+    print(admin_servicio)
+
+    print("*" * 20, "Total de Desarrollador de Soluciones Integrales: ", "*" * 20)
+    print(count_dev_solucion)
+    print("*" * 20, " DESARROLLADOR DE SOLUCIONES INTEGRALES", "*" * 20)
+    print(dev_soluciones)
+
+    print("*" * 20, "Total de Desarrollador de Sistemas Informáticos: ", "*" * 20)
+    print(count_dev_sistemas)
+    print("*" * 20, " DESARROLLADOR DE SISTEMAS INFORMATICOS ", "*" * 20)
+    print(dev_sistemas)
+
+    print("*" * 20, "Total de Investigador: ", "*" * 20)
+    print(count_investigador)
+    print("*" * 20, " INVESTIGADOR ", "*" * 20)
+    print(investigador)
+
+    print("*" * 20, "Total de Gestor de proyectos de ingenieria: ", "*" * 20)
+    print(count_gest_proyect)
+    print("*" * 20, " GESTOR DE PROYECTOS DE INGENIERIA ", "*" * 20)
+    print(gestor_proyec)
+
+    print(contador_total)
+    print(count_gest_proyect + count_desarrollador + count_admin_bd + count_admin_red + count_soporte + count_adm_servicio + count_dev_solucion + count_dev_sistemas + count_investigador)
 
 
 if __name__ == '__main__':
